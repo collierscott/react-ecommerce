3b9be9cdf5d6c9960e5c7e32e668fa6832bdc71c
Scott Collier <scott.collier@umuc.edu>
2018-10-18 14:15:57 +0200

    Should be able to query brands

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import {Box, Container, Heading} from 'gestalt';
+import Strapi from 'strapi-sdk-javascript/build/main';
 import './App.css';
 
+const apiUrl = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiUrl);
+
 class App extends Component {
+  async componentDidMount() {
+    const response = await strapi.request('POST', '/graphql', {
+      data: {
+        query: `query {
+            brands{
+            name
+            image {
+              name
+              url
+            }
+            description
+          }
+        }`
+      }
+    });
+  }
   render() {
     return (
       <Container>
