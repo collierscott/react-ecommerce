385bdb009a22a087960c36493df212baa0e3c558
Scott Collier <scott.collier@umuc.edu>
2018-10-18 15:06:40 +0200

    Should show brands

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {Box, Container, Heading} from 'gestalt';
+import {Box, Card, Container, Heading, Image, Text} from 'gestalt';
+import {Link} from 'react-router-dom';
 import Strapi from 'strapi-sdk-javascript/build/main';
 import './App.css';
 
@@ -7,23 +8,35 @@ const apiUrl = process.env.API_URL || 'http://localhost:1337';
 const strapi = new Strapi(apiUrl);
 
 class App extends Component {
+
+  state = {
+    brands: []
+  };
+
   async componentDidMount() {
-    const response = await strapi.request('POST', '/graphql', {
-      data: {
-        query: `query {
-            brands{
-            name
-            image {
+
+    try {
+      const {data} = await strapi.request('POST', '/graphql', {
+        data: {
+          query: `query {
+              brands{
               name
-              url
+              image {
+                name
+                url
+              }
+              description
             }
-            description
-          }
-        }`
-      }
-    });
+          }`
+        }
+      });
+      this.setState({brands: data.brands});
+    } catch(err) {
+      console.log(err)
+    }
   }
   render() {
+    const {brands} = this.state;
     return (
       <Container>
         {/* brands */}
@@ -36,6 +49,40 @@ class App extends Component {
           <Heading color="midnight" size="md">
             Brew Brands
           </Heading>
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="around"
+        >
+        {brands && brands.map(brand => {
+          return (
+            <Box key={brand.name} margin={2} width={200}>
+              <Card
+                image={
+                  <Box height={200} width={200}>
+                    <Image alt="Brand"
+                           naturalHeight={1}
+                           naturalWidth={1}
+                           src={`${apiUrl}${brand.image.url}`}/>
+                  </Box>
+                }
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Text bold size="xl">{brand.name}</Text>
+                  <Text align="center">{brand.description}</Text>
+                  <Text bold size="xl">
+                    <Link to={`/${brand.name}`}>See Brews</Link>
+                  </Text>
+                </Box>
+              </Card>
+            </Box>
+          )
+        })}
         </Box>
       </Container>
     );
