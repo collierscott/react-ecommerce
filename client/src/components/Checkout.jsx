baacdf3700b861381cd616698a05163a81c72f6e
Scott Collier <scott.collier@umuc.edu>
2018-10-18 12:34:03 +0200

    Add navigation and simple styling

---
+++ client/src/components/Checkout.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Checkout extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Checkout dd</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default Checkout;
