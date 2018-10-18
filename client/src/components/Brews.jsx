c554de729733af4e4177532a8951234045057225
Scott Collier <scott@onlinespaces.com>
2018-10-18 21:38:52 +0200

    Should be able to see brews for a brand

--- client/src/components/Brews.jsx
+++ client/src/components/Brews.jsx
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import Strapi from 'strapi-sdk-javascript/build/main';
+import {Box, Heading, Text, Image, Card, Button} from 'gestalt';
+import {Link} from "react-router-dom";
 
 const apiUrl = process.env.API_URL || 'http://localhost:1337';
 const strapi = new Strapi(apiUrl);
 
 class Brews extends Component {
+    state = {
+       brews: [],
+       brand: ''
+    };
+
     async componentDidMount() {
         //console.log(this.props.match.params.brandId);
         try {
-            const response = await strapi.request('POST', '/graphql', {
+            const {data} = await strapi.request('POST', '/graphql', {
                 data: {
                     query: `
                     query {
@@ -16,6 +23,7 @@ class Brews extends Component {
                         name
                         brews {
                           name,
+                          description,
                           image {
                             url
                           }
@@ -26,14 +34,95 @@ class Brews extends Component {
                     `
                 }
             });
+            this.setState({
+                brews: data.brand.brews,
+                brand: data.brand.name
+            })
         } catch (e) {
             console.log(e);
         }
     }
 
     render() {
-        return(
-            <h2>Brews</h2>
+        const {brand, brews} = this.state;
+        return (
+            <Box
+                marginTop={4}
+                display="flex"
+                justifyContent="center"
+                alignItems="start"
+            >
+                <Box
+                    display="flex"
+                    direction="column"
+                    alignItems="center"
+                >
+                    <Box margin={2}>
+                        <Heading
+                            color="orchid"
+                        >
+                            {brand}
+                        </Heading>
+                    </Box>
+                    <Box
+                        wrap
+                        dangerouslySetInlineStyle={{
+                            __style: {
+                                backgroundColor: '#bdcdd8'
+                            }
+                        }}
+                        shape="rounded"
+                        display="flex"
+                        justifyContent="center"
+                        padding={4}
+                    >
+                        {brews && brews.map(brew => {
+                            return (
+                                <Box
+                                    paddingY={4}
+                                    key={brew.name}
+                                    margin={2}
+                                    width={210}>
+                                    <Card
+                                        image={
+                                            <Box height={200} width={200}>
+                                                <Image alt="Brew"
+                                                       naturalHeight={1}
+                                                       naturalWidth={1}
+                                                       fit="cover"
+                                                       src={`${apiUrl}${brew.image.url}`}/>
+                                            </Box>
+                                        }
+                                    >
+                                        <Box
+                                            display="flex"
+                                            alignItems="center"
+                                            justifyContent="center"
+                                            direction="column"
+                                            marginBottom={2}
+                                        >
+                                            <Text bold size="xl">{brew.name}</Text>
+                                            <Text align="center">{brew.description}</Text>
+                                            <Text
+                                                align="center"
+                                                color="orchid">
+                                                ${brew.price}
+                                                </Text>
+                                            <Box
+                                                marginTop={2}
+                                                >
+                                                <Text bold size="xl">
+                                                    <Button color="blue" text="Add to Cart" />
+                                                </Text>
+                                            </Box>
+                                        </Box>
+                                    </Card>
+                                </Box>
+                            )
+                        })}
+                    </Box>
+                </Box>
+            </Box>
         )
     }
 }
