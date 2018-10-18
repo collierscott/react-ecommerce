9e8e03c14cccda0801820719fdc73f80655fd07e
Scott Collier <scott.collier@umuc.edu>
2018-10-18 13:36:45 +0200

    Brands should have a header

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import {Box, Container, Heading} from 'gestalt';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <Container>
+        {/* brands */}
+        <Box
+          display="flex"
+          justifyContent="center"
+          marginBottom={2}
+        >
+          {/* Brands Header */}
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
     );
   }
 }
