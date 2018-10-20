168d5fecccbc232ee083e3fa5cbfee2677dec794
Scott Collier <scott@onlinespaces.com>
2018-10-20 05:33:05 +0200

    Only auth users should be able to see checkout

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import 'gestalt/dist/gestalt.css';
 import App from './components/App';
 import Checkout from './components/Checkout';
@@ -8,7 +8,19 @@ import Signin from  './components/Signin';
 import Signup from './components/Signup';
 import Brews from './components/Brews';
 import Navbar from './components/Navbar';
+import {getToken} from "./utils";
 import registerServiceWorker from './registerServiceWorker';
+
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route {...rest} render={props => (
+        getToken() !== null ?
+            <Component {...props} /> :
+            <Redirect to={{
+                pathname: '/signin',
+                state: { from: props.location}
+            }} />
+    )} />
+);
 
 const Root = () => (
   <Router>
@@ -18,7 +30,7 @@ const Root = () => (
         <Route exact component={App} path="/" />
         <Route component={Signin} path="/signin" />
         <Route component={Signup} path="/signup" />
-        <Route component={Checkout} path="/checkout" />
+        <PrivateRoute component={Checkout} path="/checkout" />
         <Route component={Brews} path="/:brandId" />
       </Switch>
     </Fragment>
